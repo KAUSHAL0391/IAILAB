32964d0144ae87a5026e25de99b300773649a0c9
KAU5H4L <pacharne.ai21@bmsce.ac.in>
2023-08-30 22:08:00 +0530

    8queenproblem.py

--- 8queenproblem.py
+++ 8queenproblem.py
@@ -1,37 +1,38 @@
-def is_valid(board, row, col, n):
-    for i in range(col):
-        if board[i] == row or \
-           board[i] - i == row - col or \
-           board[i] + i == row + col:
-            return False
-    return True
+from typing import List, Tuple
 
-def solve_queen(board, col, n):
-    if col == n:
-        return True
-    
-    for row in range(n):
-        if is_valid(board, row, col, n):
-            board[col] = row
-            if solve_queen(board, col + 1, n):
-                return True
-            board[col] = -1
+# Class to represent a state in the 8-puzzle problem.
+class State:
+    def __init__(self, board: List[List[int]]):
+        self.board = board
 
-    return False
+    # Function to check if the state is the goal state.
+    def is_goal(self) -> bool:
+        goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        return self.board == goal_state
 
-def print_board(board, n):
-    for row in range(n):
-        for col in range(n):
-            if board[col] == row:
-                print('Q', end=' ')
-            else:
-                print('.', end=' ')
-        print()
-
-def solve_n_queens(n):
-    board = [-1] * n
-    if solve_queen(board, 0, n):
-        print_board(board, n)
-    else:
-        print('No solution found')
-print(solve_n_queens(8))
+    # Function to get the neighbors of the state.
+    def get_neighbors(self) -> List['State']:
+        neighbors = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == 0:
+                    # Move the blank tile up.
+                    if i > 0:
+                        new_board = [row[:] for row in self.board]
+                        new_board[i][j], new_board[i-1][j] = new_board[i-1][j], new_board[i][j]
+                        neighbors.append(State(new_board))
+                    # Move the blank tile down.
+                    if i < 2:
+                        new_board = [row[:] for row in self.board]
+                        new_board[i][j], new_board[i+1][j] = new_board[i+1][j], new_board[i][j]
+                        neighbors.append(State(new_board))
+                    # Move the blank tile left.
+                    if j > 0:
+                        new_board = [row[:] for row in self.board]
+                        new_board[i][j], new_board[i][j-1] = new_board[i][j-1], new_board[i][j]
+                        neighbors.append(State(new_board))
+                    # Move the blank tile right.
+                    if j < 2:
+                        new_board = [row[:] for row in self.board]
+                        new_board[i][j], new_board[i][j+1] = new_board[i][j+1], new_board[i][j]
+                        neighbors
